f2091112685550e256c4426383ae6db7bcae7160
Joonil Kim <bartholomaeuskim@gmail.com>
2023-01-05 16:03:01 -0700

    test

--- app/api/item_routes.py
+++ app/api/item_routes.py
@@ -19,7 +19,7 @@ def upload_item_image(name, file: FileStorage) -> str:
         aws_access_key_id = os.environ.get('S3_KEY'),
         aws_secret_access_key = os.environ.get('S3_SECRET')
     )
-    print('=========================1')
+    print(os.environ.get('S3_BUCKET'))
 
     s3.upload_fileobj(
         file,
